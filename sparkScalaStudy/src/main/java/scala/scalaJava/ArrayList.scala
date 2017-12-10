9687421bc4d754cc48f7ecc9b81902664981d468
hanlipeng <hanlipeng@qq.com>
2017-12-10 11:56:25 +0800

    java 和 scala的集合之间的隐式转换

--- sparkScalaStudy/src/main/java/scala/scalaJava/ArrayList.scala
+++ sparkScalaStudy/src/main/java/scala/scalaJava/ArrayList.scala
@@ -2,7 +2,23 @@ package scala.scalaJava
 
 /**
   * Created by root on 2017/12/9.
+  * java和scala相互转换
   */
 class ArrayList {
 
+}
+object ArrayList{
+  def main(args: Array[String]): Unit = {
+    import scala.collection.JavaConversions.bufferAsJavaList
+    import scala.collection.mutable.ArrayBuffer
+    val command = ArrayBuffer("java","c:\\test.java")
+    val processBuider = new ProcessBuilder(command)
+    val process = processBuider.start()
+    val res = process.waitFor()
+
+    import scala.collection.JavaConversions.asScalaBuffer
+    import scala.collection.mutable.Buffer
+
+    val cmd:Buffer[String] = processBuider.command()
+  }
 }
