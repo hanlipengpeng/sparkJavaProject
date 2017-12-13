89780180e504bd8140d84cffaa428bef4efe501f
hanlipeng <915619712@qq.com>
2017-12-13 17:12:57 +0800

    action操作

---
+++ sparkScalaStudy/src/main/java/javaa/spark/core/ActionReduce.java
@@ -0,0 +1,10 @@
+package javaa.spark.core;
+
+import org.apache.spark.SparkConf;
+
+public class ActionReduce {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setAppName("action").setMaster("local");
+		
+	}
+}
