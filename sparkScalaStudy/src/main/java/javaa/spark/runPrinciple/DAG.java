ccd981cce56406ee317f4dbf2461149cef7daa29
hanlipeng <915619712@qq.com>
2017-12-15 17:48:20 +0800

    架构

--- sparkScalaStudy/src/main/java/javaa/spark/runPrinciple/DAG.java
+++ sparkScalaStudy/src/main/java/javaa/spark/runPrinciple/DAG.java
@@ -1,7 +1,18 @@
 package javaa.spark.runPrinciple;
 /**
  * DAG 宽依赖和窄依赖
- * 
+ * 对于内核剖析主要是对一下的内核架构做了解
+ * Application
+ * spark-submit
+ * Driver
+ * SparkContext
+ * Master
+ * Worker
+ * Executor
+ * Job
+ * DAGSheduler
+ * TaskScheduler
+ * ShuffleMapTask and ResultTsk
  */
 public class DAG {
 
