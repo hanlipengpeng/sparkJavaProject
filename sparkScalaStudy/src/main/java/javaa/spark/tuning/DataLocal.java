31b7a1bf63bc0083f45f90dfe839cda8781581f4
hanlipeng <915619712@qq.com>
2017-12-18 17:08:28 +0800

    数据本地化策略

--- sparkScalaStudy/src/main/java/javaa/spark/tuning/DataLocal.java
+++ sparkScalaStudy/src/main/java/javaa/spark/tuning/DataLocal.java
@@ -7,6 +7,10 @@ package javaa.spark.tuning;
  * 3：NO_PREF:数据从哪里过来，都一样
  * 4：RACK_LOCAL:数据和计算它的代码再一个机架上。
  * 5：ANY:数据肯那个在任意地方，比如其他的网络环境内，或者其他的机架上。
+ * 
+ * 参数调优：
+ * spark.locality用来调节等待task可以进行数据本地化测得时间，
+ * spark.locality.wait(3000ms),spark.locality.wait.node,spark.locality.wait.process,spark.locality.wait.rack
  *
  */
 public class DataLocal {
