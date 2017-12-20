d082f71aa68839495000facddb4d8ff1f1cd0c6b
hanlipeng <hanlipeng@qq.com>
2017-12-20 21:31:17 +0800

    hadoop

---
+++ sparkScalaStudy/src/main/java/javaa/hadoop/WhatHadoop.java
@@ -0,0 +1,7 @@
+package javaa.hadoop;
+
+/**
+ * Created by root on 2017/12/20.
+ */
+public class WhatHadoop {
+}
---
+++ sparkScalaStudy/src/main/java/javaa/hadoop/mapreduce/WhatMapReduce.java
@@ -0,0 +1,7 @@
+package javaa.hadoop.mapreduce;
+
+/**
+ * Created by root on 2017/12/20.
+ */
+public class WhatMapReduce {
+}
