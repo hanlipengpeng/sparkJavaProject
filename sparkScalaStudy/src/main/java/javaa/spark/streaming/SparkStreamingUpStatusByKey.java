0d3c611c483992c1415e96173579e1744296adb7
hanlipeng <915619712@qq.com>
2017-12-29 10:19:49 +0800

    sparkStreaming upStatusByKey

--- sparkScalaStudy/src/main/java/javaa/spark/streaming/SparkStreamingUpStatusByKey.java
+++ sparkScalaStudy/src/main/java/javaa/spark/streaming/SparkStreamingUpStatusByKey.java
@@ -1,5 +1,22 @@
 package javaa.spark.streaming;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+/**
+ * sparkStreaming 状态更新
+ * @author rf
+ *
+ */
 public class SparkStreamingUpStatusByKey {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf();
+		JavaStreamingContext jssc = new JavaStreamingContext(conf,Durations.seconds(1l));
+		
+		
+		jssc.start();
+		jssc.awaitTermination();
+	}
 
 }
