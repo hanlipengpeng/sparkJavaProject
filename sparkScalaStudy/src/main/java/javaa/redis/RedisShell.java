c5ef9683e2a02d16c107fb635cbbfc46fcb85d45
hanlipengpeng <915619712@qq.com>
2017-12-31 18:18:23 +0800

    redis - shell

--- sparkScalaStudy/src/main/java/javaa/redis/RedisShell.java
+++ sparkScalaStudy/src/main/java/javaa/redis/RedisShell.java
@@ -6,6 +6,9 @@ package javaa.redis;
  *
  */
 public class RedisShell {
+
+	//flushDB清空数据
+
 	/**
 	 * String操作：
 	 * set name han  增加，修改
@@ -41,6 +44,44 @@ public class RedisShell {
 	 * hkays 返回hash里所有value
 	 * hgetall 返回hash里卖弄多有的key和value
 	 * 
+	 */
+
+
+	/**
+	 * list操作，
+	 * list是链表结构的集合，其主要功能有push，pop获取元素等
+	 * 更详细的说，list类型是一个双向链表的结构，我们可以通过相关操作进行集合的头部或者尾部添加删除元素
+	 * list的设计非常轻巧，既可以作为栈，又可以作为队列。
+	 * ipush方法：从头部加入元素（栈） 先进后出
+	 * rpush方法：从尾部加入元素（队列） 先进后出
+	 * linsert方法：插入元素
+	 * lset方法：将制定下标的元素替换
+	 * lrem方法：删除元素 返回删除的个数
+	 * ltrim方法：保留制定key的值范围内的数据
+	 * lpop方法：从list的头部删除元素，并返回删除元素
+	 * rpop方法：从list的尾部删除元素，并返回删除元素
+	 * rpoplpush方法：第一个从尾部删除元素，然后第二步从他头部加入元素
+	 * lindex方法：返回名称为key的list的index位置的元素
+	 * llen方法：返回元素的个数
+	 *
+	 *
+	 * lrange 取值
+	 * lrange list1 0 -1
+	 */
+
+	/**
+	 * set集合是string类型的无需集合，set是通过hashtable实现的，对集合我们可以取交际，并集，差集
+	 * sadd方法：项名称为key的set中添加元素
+	 * srem：删除set集合元素
+	 * scard：查看元素
+	 * spop方法：随机返回删除的key
+	 * adiff：返回两个集合的不同元素
+	 * sdiffstore方法：将返回的不同元素存储到另一个集合里
+	 *
+	 * sinter：返回集合的交集
+	 * sinterstore：返回交集结果，存入set3中
+	 * sunlon：取并集
+	 * sunionstore方法：取的并集，存入set3中
 	 */
 
 }
---
+++ sparkScalaStudy/src/main/java/javaa/redis/RedisShellHight.java
@@ -0,0 +1,8 @@
+package javaa.redis;
+
+/**
+ * 高级的shell命令使用
+ */
+public class RedisShellHight {
+
+}
