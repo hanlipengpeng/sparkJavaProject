e1da17a915c336e68e3f7a4524cec1b3f56f59d1
hanlipengpeng <915619712@qq.com>
2018-01-03 00:43:56 +0800

    向量问题

--- sparkScalaStudy/src/main/java/javaa/spark/machineLearning/mllib/DataTypes.java
+++ sparkScalaStudy/src/main/java/javaa/spark/machineLearning/mllib/DataTypes.java
@@ -2,6 +2,8 @@ package javaa.spark.machineLearning.mllib;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Matrices;
+import org.apache.spark.mllib.linalg.Matrix;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
@@ -40,9 +42,23 @@ public class DataTypes {
 	}
 	
 	/**
-	 * 局部矩阵 local matrix
+	 * 局部矩阵 local matrix,使用的是csc压缩矩阵。
 	 */
 	public static void localMatrix(){
+		Matrix dm = Matrices.dense(3,2,new double[]{1.0,2.0,3.0,4.0,5.0,6.0});
+		/**
+		 * (9.0,0.0)
+		 * (0.0,8.0)
+		 * (0.0,6.0)
+		 *  1    2
+		 *
+		 *  colPtrs:是用来计算每列有多少个数据的 colPtrs:（0,1,3）=>(1-0,3-1) = (1,2)  即第一行有一个数据，第二行有两个数据
+		 *  rowIndices:是数据在哪一行上，根据上面的一块做就是，第一列有一个数据，在第一行上，第二列有两个数据，分别在第三行和第二行。
+		 *
+		 *  参数：numrows numcols colPtrs rowLndices value
+		 */
+		Matrix sm = Matrices.sparse(3,2,new int[]{0,1,3},new int[]{0,2,1},new double[]{9,6,8});
+
 		
 	}
 	
