2d8f6ca5bad7cd65158aaf2ab6dadd95ee8f6a80
hanlipeng <915619712@qq.com>
2018-01-04 17:45:18 +0800

    机器学习支持向量机

---
+++ sparkScalaStudy/src/main/java/javaa/spark/machineLearning/mllib/SVMsTest.java
@@ -0,0 +1,9 @@
+package javaa.spark.machineLearning.mllib;
+/**
+ * SVM模型
+ * @author rf
+ *
+ */
+public class SVMsTest {
+
+}
---
+++ sparkScalaStudy/src/main/java/javaa/spark/machineLearning/mllib/WhatClassificationAndRegression.java
@@ -0,0 +1,18 @@
+package javaa.spark.machineLearning.mllib;
+/**
+ * 分类和回归模型
+ * 1：线性模型
+ *   1.1：线性分类(逻辑回归)
+ *   1.2：线性回归
+ * 2：决策树
+ * 3:决策树组合
+ *   3.1：随机森林
+ *   3.2： 梯度推动树
+ * 4：朴素贝叶斯
+ * 5：保序回归
+ * 
+ *
+ */
+public class WhatClassificationAndRegression {
+
+}
