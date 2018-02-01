138fb52b75e62d94f09aee1e1af2ed6356936861
hanlipeng <915619712@qq.com>
2018-02-01 16:33:20 +0800

    als

--- sparkScalaStudy/src/main/java/javaa/spark/machineLearning/mllib/collaborativeFiltering/ALSTest.java
+++ sparkScalaStudy/src/main/java/javaa/spark/machineLearning/mllib/collaborativeFiltering/ALSTest.java
@@ -17,6 +17,9 @@ import scala.Tuple2;
 /**
  * 协同过滤  推荐算法
  * @author 韩利鹏
+ * 
+ * 数据格式是  user product rating
+ * 先训练模型，然后使用 user和product进行预测，然后用预测和rating进行对比
  *
  */
 public class ALSTest {
@@ -25,10 +28,14 @@ public class ALSTest {
         JavaSparkContext jsc = new JavaSparkContext(conf);
         String path = SVMsTest.class.getClassLoader().getResource("als/test.txt").getPath();
         
+        //对数据进行分割处理
         JavaRDD<String> data = jsc.textFile(path);
+        //user product rating
         JavaRDD<Rating> ratings = data.map(
 		  new Function<String, Rating>() {
-		    public Rating call(String s) {
+			private static final long serialVersionUID = 1L;
+
+			public Rating call(String s) {
 		      String[] sarray = s.split(",");
 		      return new Rating(Integer.parseInt(sarray[0]), Integer.parseInt(sarray[1]),
 		        Double.parseDouble(sarray[2]));
@@ -42,7 +49,9 @@ public class ALSTest {
         MatrixFactorizationModel model = ALS.train(JavaRDD.toRDD(ratings), rank, numInterations,0.01);
         
         //Evaluate the model on rating using ALS
+        //用户和产品
         JavaRDD<Tuple2<Object,Object>> userProducts = ratings.map(new Function<Rating, Tuple2<Object,Object>>() {
+			private static final long serialVersionUID = 1L;
 
 			@Override
 			public Tuple2<Object, Object> call(Rating v1) throws Exception {
@@ -52,6 +61,7 @@ public class ALSTest {
 		});
         
         JavaRDD<Tuple2<Tuple2<Integer, Integer>, Double>> map = model.predict(JavaRDD.toRDD(userProducts)).toJavaRDD().map(new Function<Rating, Tuple2<Tuple2<Integer,Integer>,Double>>() {
+			private static final long serialVersionUID = 1L;
 
 			@Override
 			public Tuple2<Tuple2<Integer, Integer>, Double> call(Rating r)
@@ -59,8 +69,11 @@ public class ALSTest {
 				return new Tuple2<Tuple2<Integer,Integer>, Double>(new Tuple2<Integer,Integer>(r.user(), r.product()),r.rating());
 			}
 		});
+        
+        //用户和推荐产品
         JavaPairRDD<Tuple2<Integer,Integer>,Double> predictions = JavaPairRDD.fromJavaRDD(map);
         
+        //产品和推荐产品
         JavaRDD<Tuple2<Double,Double>> ratesAndPreds = 
         		JavaPairRDD.fromJavaRDD(ratings.map(new Function<Rating, Tuple2<Tuple2<Integer,Integer>,Double>>() {
 					private static final long serialVersionUID = 1L;
@@ -86,8 +99,9 @@ public class ALSTest {
         //dave and load model
         String savePath = "F:\\tmp\\model\\als";
         model.save(jsc.sc(), savePath);
-        MatrixFactorizationModel sameModel = MatrixFactorizationModel.load(jsc.sc(),savePath);
-
+        //MatrixFactorizationModel sameModel = MatrixFactorizationModel.load(jsc.sc(),savePath);
+        jsc.close();
+        
     }
 
 }
